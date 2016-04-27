dc69c1ac59e8647b71a881b60b72a005703a75bd
ansaris <salman.ansari.usa@gmail.com>
2016-04-27 15:55:36 -0500

    Setup ScrapeLibrary and ScrapeClient in one project.

---
+++ ScrapeClient/Program.cs
@@ -0,0 +1,21 @@
+﻿using ScrapeLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScrapeClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Scrape myScrape = new Scrape();
+            //string value = myScrape.ScrapeWebpage("http://msdn.microsoft.com");
+            string value = myScrape.ScrapeWebpage("http://msdn.microsoft.com", @"c:\users\public\testfolder\html2.txt");
+            Console.WriteLine(value);
+            Console.ReadLine();
+        }
+    }
+}
